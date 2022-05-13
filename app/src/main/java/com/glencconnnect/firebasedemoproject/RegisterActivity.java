82446b7b27e1382e839c12ec724d21f511a6dd5f
glenchiridza <glenchiridz@gmail.com>
2022-05-13 23:12:52 +0200

    auth activities

---
+++ app/src/main/java/com/glencconnnect/firebasedemoproject/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.glencconnnect.firebasedemoproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
