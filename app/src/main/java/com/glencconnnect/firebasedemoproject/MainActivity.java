bd6c3c2cdca222d990d62eed25c8eab8de169050
glenchiridza <glenchiridz@gmail.com>
2022-05-13 23:11:50 +0200

    view

--- app/src/main/java/com/glencconnnect/firebasedemoproject/MainActivity.java
+++ app/src/main/java/com/glencconnnect/firebasedemoproject/MainActivity.java
@@ -3,12 +3,18 @@ package com.glencconnnect.firebasedemoproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnRegister;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnLogin);
     }
 }
