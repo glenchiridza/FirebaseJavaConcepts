6e4d20cd59ba99e2113210ed65a286e0e1c1f694
glenchiridza <glenchiridz@gmail.com>
2022-05-13 23:14:02 +0200

    Add intents

--- app/src/main/java/com/glencconnnect/firebasedemoproject/MainActivity.java
+++ app/src/main/java/com/glencconnnect/firebasedemoproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.glencconnnect.firebasedemoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
 
         btnRegister = findViewById(R.id.btnRegister);
         btnLogin = findViewById(R.id.btnLogin);
+
+        btnRegister.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            finish();
+        });
+        btnLogin.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            finish();
+        });
     }
 }
