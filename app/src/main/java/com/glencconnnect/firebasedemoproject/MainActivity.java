488b6646b417eb0762e5e9696559300c2ffd990d
glenchiridza <glenchiridz@gmail.com>
2022-05-14 10:25:19 +0200

    Sending data to db

--- app/src/main/java/com/glencconnnect/firebasedemoproject/MainActivity.java
+++ app/src/main/java/com/glencconnnect/firebasedemoproject/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -19,6 +21,10 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnlogout;
+
+    private EditText edtBookName;
+    private EditText edtAuthorName;
+    private Button btnSave;
 
     private FirebaseAuth mAuth;
 
@@ -32,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
 
         btnlogout = findViewById(R.id.btnLogout);
 
+        edtAuthorName = findViewById(R.id.txtAuthor);
+        edtBookName = findViewById(R.id.txtBookName);
+        btnSave = findViewById(R.id.btnSave);
+
         mAuth = FirebaseAuth.getInstance();
 
         btnlogout.setOnClickListener(v->{
@@ -43,11 +53,26 @@ public class MainActivity extends AppCompatActivity {
         dbRef = fbDatabase.getReference().child("Books").child("Android-Java");
 
         HashMap<String,Object> map = new HashMap<>();
-        map.put("name","Introduction to Java");
-        map.put("author","Glen Chiridza");
+//        map.put("name","Introduction to Java");
+//        map.put("author","Glen Chiridza");
 
         dbRef  =fbDatabase.getReference().child("Books").child("Android-Java");
 
         dbRef.updateChildren(map);
+
+        btnSave.setOnClickListener(v->{
+            String txt_book_name = edtBookName.getText().toString();
+            String txt_author_name = edtBookName.getText().toString();
+            if(txt_book_name.isEmpty() || txt_author_name.isEmpty()){
+                Toast.makeText(MainActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
+            }else{
+                map.put("name",txt_book_name);
+                map.put("author",txt_author_name);
+                dbRef.updateChildren(map)
+                        .addOnSuccessListener(aVoid -> {
+                            Toast.makeText(MainActivity.this, "Data Saved to DB", Toast.LENGTH_SHORT).show();
+                        });
+            }
+        });
     }
 }
